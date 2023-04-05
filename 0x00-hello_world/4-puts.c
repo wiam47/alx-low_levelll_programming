5d76b377b8166000deb4c5bffd491e20c059948d
Wiam BISSI <86968238+wiam47@users.noreply.github.com>
2023-04-05 14:43:10 +0000

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
-* main - Main function return int value
-*return : always 0 success
+	*main - Main function return int value
+	*return : always 0 success
 */
 int main(void)
 {
